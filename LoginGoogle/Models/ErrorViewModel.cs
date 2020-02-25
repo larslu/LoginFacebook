877e8702d99a59249b3101821667ee9803bb2bee
Lars Lundin <larslundin72@live.se>
2020-02-25 23:14:26 +0100

    Add project files.

---
+++ LoginGoogle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LoginGoogle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
